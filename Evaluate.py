3853d3b4ba389f5a95fd01e9295264c43000836d
chenhaoling <18395589135@163.com>
2022-06-14 08:22:00 +0800

    add get tier2-tier3-job-info function.

--- Evaluate.py
+++ Evaluate.py
@@ -8,13 +8,13 @@
 @desc: 
 """
 from simpletransformers.simpletransformers.model import TransformerModel
-
+from simpletransformers.classification.classification_model import ClassificationModel
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 import numpy as np
-data = pd.read_csv("./data/train_set_10w.csv")
+data = pd.read_csv("./data/train_set_6w.csv")
 
 
 le = preprocessing.LabelEncoder()
@@ -26,7 +26,7 @@ print(np_labels)
 train_texts, val_texts, train_labels, val_labels = train_test_split(data['jobDesc'], data['jobPosition'], test_size=.2, random_state=0)
 
 print('train performance:', end='\n')
-model = TransformerModel('distilbert', './distilbert_outputs/best_model/', num_labels=np_labels)
+model = ClassificationModel('distilbert', './distilbert_outputs/best_model/', num_labels=np_labels)
 train_y, _ = model.predict(train_texts.tolist())
 print(classification_report(le.inverse_transform(train_labels),le.inverse_transform(train_y)))
 
